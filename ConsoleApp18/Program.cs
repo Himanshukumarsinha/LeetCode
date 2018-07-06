9efe55da016f924f89dc5972c453a3ab78156559
Himanshu Kumar Sinha <thisishimanshu@live.com>
2018-07-05 21:07:15 -0700

    updated

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -4,12 +4,108 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp18
+namespace LinkedList
 {
     class Program
     {
+        public class Node
+        {
+           public int data;
+           public Node next;
+           public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+
+
+        }
+        public class mylinkedlist
+        {
+            Node HeadNode = new Node(0);
+            public mylinkedlist(int a)
+            {
+                HeadNode.data = a;
+                HeadNode.next = null;
+            }
+
+            public void AddNodeAtTheBeginning(int d)
+            {
+              
+
+                Node temp = new Node(d);
+                temp.next = HeadNode;
+                HeadNode = temp;
+
+               
+            }
+
+            public void AddNodeAtTheEnd(int d)
+            {  
+                    if (HeadNode.next == null)
+                    {
+                   
+                    Node temp = new Node(d);
+                    HeadNode.next = temp;
+                       
+                    }
+                    else
+                    { 
+                        Node current = HeadNode;
+                        while (current.next != null)
+                        {
+                        current = current.next;
+                      
+                        }
+                    Node temp = new Node(d);
+                    current.next = temp;
+                }
+            }
+            public void PrintlinkedList()
+            {
+                Node current = HeadNode;
+                if (current == HeadNode)
+                {
+                    Console.Write("{0}|->>", current.data);                    
+                }
+                while ( current.next != null)
+               
+                {
+                    current = current.next;
+                    Console.Write("{0}|->>", current.data);
+               
+                }
+            }
+
+            public void PrintlinkedList2()
+            {               
+                Node current = HeadNode;
+                {                   
+                    Console.Write("{0}|->>", current.data);                  
+                }
+                while (current.next != null)
+                {
+                    current = current.next;
+                    Console.Write("{0}|->>", current.data);
+
+                }
+            }
+
+
+
+        }
+
         static void Main(string[] args)
         {
+            mylinkedlist a = new mylinkedlist(500);
+            a.AddNodeAtTheBeginning(100);
+            a.AddNodeAtTheBeginning(200);
+            a.AddNodeAtTheBeginning(300);
+            a.AddNodeAtTheBeginning(30780);
+            a.AddNodeAtTheEnd(3078000);
+           // a.PrintlinkedList();
+            a.PrintlinkedList2();
+
         }
     }
 }
